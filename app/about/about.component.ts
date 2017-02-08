95e17f80a0fd793d44f071d1fad860a83c63ff55
artbat <abatanero@paradigmadigital.com>
2017-02-08 18:41:00 +0100

    Creamos dos componentes adicionales: AboutComponent y SearchComponent y los agregamos al AppComponent

---
+++ app/about/about.component.ts
@@ -0,0 +1,9 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+   moduleId: module.id,
+   selector: 'about',
+   templateUrl: 'about.component.html'
+})
+export class AboutComponent {}
--- app/app.module.ts
+++ app/app.module.ts
@@ -4,10 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { AboutComponent } from './about/about.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
-   imports: [BrowserModule],
-   declarations: [AppComponent, NavbarComponent],
-   bootstrap: [AppComponent]
+   imports: [
+      BrowserModule
+   ],
+   declarations: [
+      AppComponent,
+      NavbarComponent,
+      AboutComponent,
+      SearchComponent ],
+   bootstrap: [
+      AppComponent
+   ]
 })
 export class AppModule {}
