0e2644d3b63fb843554c7d44e15ba4b3a965ed40
artbat <abatanero@paradigmadigital.com>
2017-02-08 18:32:45 +0100

    Añadimos el componente navbarComponent y lo convertimos en hijo del AppComponent

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,11 +1,13 @@
 
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
    imports: [BrowserModule],
-   declarations: [AppComponent],
+   declarations: [AppComponent, NavbarComponent],
    bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ app/navbar/navbar.component.ts
@@ -0,0 +1,11 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+   moduleId: module.id,
+   selector: 'navbar',
+   templateUrl: 'navbar.component.html'
+})
+export class NavbarComponent {
+   projectName:string = 'mySpotify'
+}
